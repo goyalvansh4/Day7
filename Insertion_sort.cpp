fa9753aa533e18da51ac03303a00706b53e53e7b
Vansh Gupta <97226346+goyalvansh4@users.noreply.github.com>
2022-01-12 20:44:23 +0530

    Create Insertion_sort.cpp

---
+++ Insertion_sort.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std; 
+
+int main(){ 
+
+int n;
+cin>>n; 
+int arr[n]; 
+for(int i=0;i<n;i++){
+cin>>arr[i]; 
+} 
+
+for(int i=1;i<n;i++){ 
+int current = arr[i];
+int j = i+1;
+while(arr[j]>current && j>0){ 
+  
